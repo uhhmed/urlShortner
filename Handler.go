73777e1b3774ebd6b1eaa660e0d31a6e069b6509
uhhmed <uhhmed@Ahmeds-MacBook-Pro-2.local>
2020-09-09 04:25:42 -0700

    initial commit

---
+++ Handler.go
@@ -0,0 +1,53 @@
+package shortener
+
+import (
+	"net/http"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// get the path part of the url only
+		path := r.URL.Path
+
+		// assert there is a matching path in the map that was passed
+		if dest, ok := pathsToUrls[path]; ok {
+
+			// if path is in map, redirect to url
+			http.Redirect(w, r, dest, http.StatusFound)
+
+			// return to make sure rest of func doesn't run
+			return
+		}
+
+		// if there is no mathing path, fallback.
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// TODO: Implement this...
+	return nil, nil
+}
---
+++ main/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	mux := defaultMux()
+
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+
+	mapHandler := shortener.MapHandler(pathsToUrls, mux)
+
+	http.ListenAndServe(":8080", mapHandler)
+
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
